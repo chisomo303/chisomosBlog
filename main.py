4d65cf497bb5fb37eec51e61fe51decf5a417cea
Chisomo Malembeka <chisomomalembeka@gmail.com>
2024-03-15 12:18:10 +0200

    create Procfile

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import smtplib
+import os
 
 
 '''
@@ -28,12 +29,12 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
-EMAIL = "chisomomalembeka@gmail.com"
-PASSWORD = "pujd yebf dkws ygxn"
+EMAIL = os.environ.get("MY_EMAIL")
+PASSWORD = os.environ.get("MY_PASSWORD")
 
 # TODO: Configure Flask-Login
 login_manager = LoginManager()
@@ -52,7 +53,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -256,7 +257,6 @@ def contact():
         email = request.form["email"]
         phone_no = request.form["phone"]
         message = request.form["message"]
-        print(f"{name}\n{email}\n{phone_no}\n{message}")
 
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
             connection.starttls()
@@ -269,4 +269,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5004)
+    app.run(debug=False, port=5004)
